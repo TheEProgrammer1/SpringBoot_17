d3ae17ff64a7c7c2588495fbd3ba9c0233c78a3d
GBTC440005ur <EEnamor1@mongtomerycollege.edu>
2018-03-13 13:39:15 -0400

    extending security by adding specific users

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -14,10 +14,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        http.authorizeRequests().antMatchers("/")
-                                .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')").antMatchers("/admin")
-                                .access("hasRole('ROLE_ADMIN')").anyRequest().authenticated()
-                                .and().formLogin().loginPage("/login").permitAll().and().httpBasic();
+        http.authorizeRequests()
+                .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().authenticated()
+            .antMatchers("/")
+            .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+            .and()
+            .formLogin().loginPage("/login").permitAll()
+            .and()
+            .httpBasic();
     }
 
     @Override
---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "USER_DATA")
+public class User
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @Column(name = "username")
+    private String username;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+}
